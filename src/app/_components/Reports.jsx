9cee55165e1567bdf4b64fdf1ce6199623cee6fe
yousafsabir <myousafdev@gmail.com>
2024-03-04 15:56:53 +0500

    feat: added transformations to reports section

--- src/app/_components/Reports.jsx
+++ src/app/_components/Reports.jsx
@@ -5,7 +5,7 @@ export default function Reports() {
         <section className="py-[70px] px-[30px] bg-reports-bg">
             <div className="px-[40px] pl-[280px] bg-white py-[60px] max-w-[1512px] rounded-[25px] mx-auto">
                 <div className="max-w-[1209px] flex gap-[266px] mx-auto items-center">
-                    <div className="min-w-[350px] h-[550px] bg-cover bg-center scale-[1.25] relative bg-reports-card-bg">
+                    <div className="min-w-[350px] group h-[550px] bg-cover bg-center scale-[1.25] relative bg-reports-card-bg">
                         <Image
                             className="absolute top-[60%] -translate-y-[60%] -right-[15%] translate-x-[15%]"
                             src="/reports-pic-1.png"
@@ -14,14 +14,14 @@ export default function Reports() {
                             alt="Reports Illustrations"
                         />
                         <Image
-                            className="absolute top-[60%] -translate-y-[60%] left-1/2 -translate-x-1/2"
+                            className="absolute top-[60%] group-hover:left-[350%] group-hover:-translate-x-[350%] -translate-y-[60%] left-1/2 -translate-x-1/2 transition-all duration-500"
                             src="/reports-pic-2.png"
                             width={328}
                             height={428}
                             alt="Reports Illustrations"
                         />
                         <Image
-                            className="absolute top-[60%] -translate-y-[60%] -left-[20%] -translate-x-[20%]"
+                            className="absolute top-[60%] -translate-y-[60%] -left-[20%] -translate-x-[20%] group-hover:-left-[28%] group-hover:-translate-x-[28%] transition-all duration-500"
                             src="/reports-pic-3.png"
                             width={377}
                             height={452}
