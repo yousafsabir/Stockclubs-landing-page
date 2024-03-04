aaddb19aa44703c603d299faf448c6f534a47fd5
yousafsabir <myousafdev@gmail.com>
2024-03-04 17:53:18 +0500

    feat: failed attempt at adding font

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,14 @@
 import Navbar from "./_components/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import LocalFont from "next/font/local";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = LocalFont({
+    src: "../../public/fonts/roboto.woff2",
+    weight: ["100", "300", "400", "500", "700", "900"],
+});
+
+// const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
     title: "Stock Clubs",
     description: "By M.Noman for Softlix",
@@ -11,7 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body className={`${inter.className} text-primary`}>
+            <body className={`${roboto.className} text-primary`}>
                 <Navbar />
                 {children}
             </body>
